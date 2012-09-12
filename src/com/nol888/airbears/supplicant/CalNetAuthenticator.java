a2e690e9220b744f264fd637d59fc0c14bff236f
Nolan Lum <nol888@gmail.com>
2012-09-11 20:16:44 -0700

    More CalNet stuff.

--- src/com/nol888/airbears/supplicant/CalNetAuthenticator.java
+++ src/com/nol888/airbears/supplicant/CalNetAuthenticator.java
@@ -3,10 +3,22 @@ package com.nol888.airbears.supplicant;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import android.net.http.AndroidHttpClient;
 import android.util.Log;
 
 public class CalNetAuthenticator {
 	private static final String LOGTAG = "AB_SUPP::CALNET";
+	private static final String CAS_NO_REDIR_URL = "https://auth.berkeley.edu/cas/login";
+	private static final String CAS_URL = "https://auth.berkeley.edu/cas/login?service=https%3a%2f%2fwlan.berkeley.edu%2fcgi-bin%2flogin%2fcalnet.cgi%3fsubmit%3dCalNet%26url%3d";
+	private static final String WLAN_LANDING_URL = "https://wlan.berkeley.edu/cgi-bin/login/calnet.cgi?url=&count=1";
+	
+	private static final CookieStore cookies = new BasicCookieStore();
 
 	private CalNetAuthenticator() {
 		throw new IllegalStateException("Don't do this.");
@@ -32,6 +44,12 @@ public class CalNetAuthenticator {
 	}
 
 	public static void authenticate(String id, String password, boolean cas_no_redir) {
+		String cas_url = cas_no_redir ? CAS_NO_REDIR_URL : CAS_URL;
+		AndroidHttpClient opener = AndroidHttpClient.newInstance(System.getProperty("http.agent"));
+		opener.enableCurlLogging(LOGTAG, Log.DEBUG);
+		
+		HttpContext ctx = new BasicHttpContext();
+		ctx.setAttribute(ClientContext.COOKIE_STORE, cookies);
 
 	}
 }
