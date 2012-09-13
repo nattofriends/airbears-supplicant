60ae63f0119353ece0cb0c452299f80049d66f42
nattofriends <nattofriends@gmail.com>
2012-09-12 19:16:31 -0700

    Embarassing fixes.

--- build_win32.py
+++ build_win32.py
@@ -9,7 +9,7 @@ except ImportError:
     sys.exit(-1)
     
 if wx.VERSION[0:2] < (2, 9):
-    print "wxPython 2.9 or above is required.")
+    print "wxPython 2.9 or above is required."
     sys.exit(-1)
 
 manifest = """
